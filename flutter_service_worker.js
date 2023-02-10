08e9243a18f2cbe4097b57d7200db817ec11bb16
Shelley Wong <swong26@csuchico.edu>
2023-02-10 14:47:22 -0800

    adds shared prefs example to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "53fb59e54fb87bb44dba7c2200e1dead",
-"index.html": "051962d435209f0f5a416e8654cee3ff",
-"/": "051962d435209f0f5a416e8654cee3ff",
-"main.dart.js": "09c2aa29e596e03c5652a36a11d5f9e0",
+"index.html": "5a6b5e52aeea766fec5d30363639debf",
+"/": "5a6b5e52aeea766fec5d30363639debf",
+"main.dart.js": "7377cf04d306acd322612b7f14cddc2b",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "ace26d0c71a6ddf01f5f72671a426a89",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "259068529bed6e57beac74e9760f7147",
+"assets/NOTICES": "b282e5e401be9730656162b4e438b4ca",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
