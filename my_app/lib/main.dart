b57c53d5db147645c66a4e62c091fe81889bd3ab
Shelley Wong <swong26@csuchico.edu>
2023-02-10 14:45:44 -0800

    adds shared preferences example

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,29 +49,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future<int> _counter;
+  //int _counter = 0;
 
-  void _incrementCounter() {
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    final int counter = (prefs.getInt('counter') ?? 0) + 1;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = prefs.setInt('counter', counter).then((bool success){
+        return counter;
+      });
     });
   }
 
-  void _decrementCounter() {
+  Future<void> _decrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    final int counter = (prefs.getInt('counter') ?? 0) - 1;
     setState(() {
-      _counter--;
+      _counter = prefs.setInt('counter', counter).then((bool success){
+        return counter;
+      });
     });
   }
 
   @override
   void initState() {
     super.initState();
-    _counter = 0;
+    _counter = _prefs.then((SharedPreferences prefs){
+      return prefs.getInt('counter') ?? 0;
+    });
   }
 
   @override
@@ -122,12 +130,26 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Icon(Icons.thumb_up),
                     ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(12.0,0.0,12.0,0.0),
-                    child: Text(
-                      '$_counter',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
+                  FutureBuilder(
+                    future: _counter,
+                    builder: (BuildContext context, AsyncSnapshot<int> snapshot){
+                      switch(snapshot.connectionState){
+                        case ConnectionState.waiting:
+                          return const CircularProgressIndicator();
+                        default:
+                          if(snapshot.hasError){
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            return Container(
+                              padding: const EdgeInsets.fromLTRB(12.0,0.0,12.0,0.0),
+                              child: Text(
+                                'Counter: ${snapshot.data}',
+                                style: Theme.of(context).textTheme.headlineMedium,
+                              ),
+                            );
+                          }
+                      }
+                    },
                   ),
                   IconButton(
                     icon: const Icon(Icons.thumb_down),
