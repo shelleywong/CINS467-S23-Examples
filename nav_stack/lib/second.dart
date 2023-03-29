09c58fdeec94db837c60e0c11b3e672f3b04142a
Shelley Wong <swong26@csuchico.edu>
2023-03-29 14:13:17 -0700

    adds simple navigation example

---
+++ nav_stack/lib/second.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'components/drawer.dart';
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      drawer: getDrawer(context),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
